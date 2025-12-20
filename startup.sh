8b11d93a6befc0b7cc474df3a7e38d3684e8c671
Riley-Cotter <rileyjosephcotter@gmail.com>
2025-12-20 11:57:27 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,22 +5,22 @@ echo -e "\n== Startup initiated: $(date) ==" > "$LOG_FILE"
 
 # Mount USB
 echo -e "Start USB MOUNT" >> "$LOG_FILE"
-/home/ri/Pi_Video_CRT_Player/mount_usb.sh >> "$LOG_FILE" 2>&1 &
+/home/ri/Christmas_Mini_TV/mount_usb.sh >> "$LOG_FILE" 2>&1 &
 
 # Pull latest repo update
 /bin/sleep 5
 echo -e "Pull Repo" >> "$LOG_FILE"
-/home/ri/Pi_Video_CRT_Player/pull_repo.sh >> "$LOG_FILE" 2>&1 
+/home/ri/Christmas_Mini_TV/pull_repo.sh >> "$LOG_FILE" 2>&1 
 
 # Start background image script
 #/bin/sleep 5
 #echo -e "Start Background Image" >> "$LOG_FILE"
-#/usr/bin/python3 /home/ri/Pi_Video_CRT_Player/background_image.py >> "$LOG_FILE" 2>&1 &
+#/usr/bin/python3 /home/ri/Christmas_Mini_TV/background_image.py >> "$LOG_FILE" 2>&1 &
 
 # Start video player
 /bin/sleep 5
 echo -e "Start Video Player" >> "$LOG_FILE"
-/usr/bin/python3 /home/ri/Pi_Video_CRT_Player/video_player.py >> "$LOG_FILE" 2>&1 &
+/usr/bin/python3 /home/ri/Christmas_Mini_TV/video_player.py >> "$LOG_FILE" 2>&1 &
 
 # Optional: keep script alive to prevent service from exiting (if needed)
 wait
