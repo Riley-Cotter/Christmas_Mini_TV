8722a95bf6b0e36bef6810c86a9b1d24af408f24
Riley-Cotter <rileyjosephcotter@gmail.com>
2025-12-20 12:30:42 -0600

    Add start time parameter to media_new
    Set start time for video playback to 100 seconds.

--- video_player.py
+++ video_player.py
@@ -18,7 +18,7 @@ while True:
         print(f"Playing: {video}")
 
         player = instance.media_player_new()
-        media = instance.media_new(video)
+        media = instance.media_new(video, f'start-time={100}')
         player.set_media(media)
 
         player.video_set_aspect_ratio("3:4")
