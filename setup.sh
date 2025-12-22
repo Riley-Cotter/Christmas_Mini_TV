c5df5dbee2fd81c2205fdff9ab16dec77fbd773e
Riley-Cotter <rileyjosephcotter@gmail.com>
2025-12-22 00:19:40 -0600

    Add installation of raspi-gpio package

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ sudo apt install -y python3-pip
 sudo apt-get install -y vlc
 sudo sed -i 's/geteuid/getppid/' /usr/bin/vlc
 sudo apt install -y python3-vlc
+sudo apt-get install raspi-gpio
 
 #Set shell scripts code to executable
 chmod +x /home/ri/Christmas_Mini_TV/*.sh
