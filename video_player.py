4f3af6646947d2e154468e24f8d687a56a2499f1
Riley-Cotter <rileyjosephcotter@gmail.com>
2025-12-22 12:17:59 -0600

    Implement button control for video playback
    Added GPIO button functionality to control video playback.

--- video_player.py
+++ video_player.py
@@ -1,10 +1,22 @@
 import os
 import vlc
 import time
+import RPi.GPIO as GPIO
 
+# Use the BCM pin numbering scheme (GPIO numbers, not physical pin numbers)
+GPIO.setmode(GPIO.BCM)
+
+# Define the GPIO pin the button is connected to
+BUTTON_PIN = 26
+
+# Set up the button pin as an input, and enable the internal pull-up resistor
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Videos location and types:
 VIDEO_DIR = "/home/ri/videos/usb"
 VIDEO_EXT = (".mp4", ".mkv", ".avi", ".mov")
 
+# Create Video Instance
 instance = vlc.Instance("--aout=alsa")
 
 while True:
@@ -34,6 +46,7 @@ while True:
 
         # --- MONITOR UNTIL VIDEO ENDS ---
         while True:
+            GPIO.add_event_detect(BUTTON_PIN, GPIO.FALLING, callback=button_callback, bouncetime=200)
             state = player.get_state()
             if state in (vlc.State.Ended, vlc.State.Error, vlc.State.Stopped):
                 break
@@ -42,3 +55,7 @@ while True:
         # Give VLC time to release the file
         player.stop()
         time.sleep(1)
+        
+def button_callback(channel):
+    player.stop()
+    print("Button was pressed!")
